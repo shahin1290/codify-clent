acfd74d234d1a340eec45fab9cb655b057658c24
shahin1290 <s.patowary@yahoo.com>
2022-11-12 00:00:28 +0100

    adds scss workflow and general stylings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,9 @@
-import React from "react";
-import Recipes from "./Recipes";
-import sword from "../images/swc-sword.png";
-import swordSvg from "../images/sword.svg";
-
 const App = () => {
-
-  
   return (
     <>
-      <section className="hero"></section>
       <main>
         <section>
           <h1>Oh Herro, React.</h1>
-          <img src={sword} alt="sword" width="250" />
-          <img src={swordSvg} alt="sword" width="250" />
-          <Recipes />
         </section>
       </main>
     </>
